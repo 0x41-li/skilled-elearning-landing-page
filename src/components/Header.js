d1b302986e75cf568b1040b0bd36751b6e7f920a
Ali Sbane <ali.sbane.dev@gmail.com>
2022-05-16 21:02:53 +0100

    Animation added with GSAP library, I will still make some tweaks, to make it all dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+import gsap from "gsap";
 
 // scss
 import "../scss/header.scss";
 
 export default function Header() {
+  const header = useRef();
+  const logoText = useRef();
+  const cta = useRef();
+
+  // gsap timeline
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    const headerStyle = window.getComputedStyle(header.current);
+
+    const headerMarginLeftPlusLogoTextWidth =
+      parseInt(headerStyle.marginLeft.replace("px", "")) +
+      logoText.current.clientWidth;
+
+    tl.from(header.current, { autoAlpha: 0 })
+      .from(logoText.current, {
+        x: -headerMarginLeftPlusLogoTextWidth,
+        duration: 0.6,
+      })
+      .from(cta.current, {
+        y: "-200",
+        duration: 0.6,
+      }, 0.2);
+  }, []);
+
   return (
-    <header>
+    <header ref={header}>
       <div className="logo">
-        <p className="logo__text">skilled</p>
+        <p className="logo__text" ref={logoText}>
+          skilled
+        </p>
       </div>
       <div className="header__cta-wrapper">
-        <button className="header__cta">Get Started</button>
+        <button className="header__cta" ref={cta}>
+          Get Started
+        </button>
       </div>
     </header>
   );
