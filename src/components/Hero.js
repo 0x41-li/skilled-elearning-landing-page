38271b66b9cbc2bb1b38e0013fb970f8f29d383f
Ali Sbane <ali.sbane.dev@gmail.com>
2022-05-19 19:14:51 +0100

    Animation added to all hero elements

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+// gsap
+import gsap from "gsap";
 
 // images
 import heroImgMobile from "../assets/images/hero/circle-wrapper-with-female-mobile.png";
@@ -10,22 +13,49 @@ import gradientRectangleDesktop from "../assets/images/hero/gradient-rectangle-o
 // import scss
 import "../scss/hero.scss";
 
-export default function () {
+export default function Hero() {
+  // DOM references
+  const hero = useRef();
+  const h1 = useRef();
+  const heroPara = useRef();
+  const heroBtn = useRef();
+  const heroImgWrapper = useRef();
+
+  // gsap timeline
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.from(hero.current, { autoAlpha: 0, delay: 1 })
+      .from(
+        [h1.current, heroPara.current, heroBtn.current, heroImgWrapper.current],
+        { y: -40, opacity: 0, stagger: 0.2 },
+        1.4
+      )
+      .from(
+        heroImgWrapper.current,
+        {
+          x: 400
+        },
+        2
+      );
+  }, []);
   return (
-    <div className="hero">
+    <div className="hero" ref={hero}>
       {/* hero text part */}
       <div className="hero__text-part">
-        <h1>Maximize skill, minimize budget</h1>
-        <p className="hero__p">
+        <h1 ref={h1}>Maximize skill, minimize budget</h1>
+        <p className="hero__p" ref={heroPara}>
           Our modern courses across a range of in-demand skills will give you
           the knowledge you need to live the life you want.
         </p>
-        <button className="hero__btn">Get Started</button>
+        <button className="hero__btn" ref={heroBtn}>
+          Get Started
+        </button>
       </div>
 
       {/* Hero illustration part */}
       <div className="hero__illustration-part">
-        <div className="hero__img-wrapper">
+        <div className="hero__img-wrapper" ref={heroImgWrapper}>
           <div className="hero__members">
             <p className="hero__members-p">Members</p>
             <p className="hero__members-num">29k</p>
